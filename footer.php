f0b0116eb4e5cce29ccaa804eb574b0a30db8a4d
DafneCruz2003 <comprasfamiliares24@gmail.com>
2025-09-27 09:49:43 -0600

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,41 @@
+ <!-- Footer -->
+<footer class="text-center">
+  <link rel="stylesheet" href="<?= base_url() ?>assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <a class="up-arrow" href="#myPage" data-toggle="tooltip" title="TO TOP">
+    <span class="glyphicon glyphicon-chevron-up"></span>
+  </a><br><br>
+  
+    <!-- Footer -->
+    <footer>
+        <div class="footer-container">
+
+            <div class="footer-col">
+                <h4>Sobre EduGlobal</h4>
+                <p>Plataforma educativa que conecta a estudiantes con cursos prácticos en distintas áreas del conocimiento.</p>
+            </div>
+
+            <div class="footer-col">
+                <h4>Enlaces Rápidos</h4>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Cursos</a></li>
+                    <li><a href="#">Instructores</a></li>
+                    <li><a href="#">Contacto</a></li>
+                </ul>
+            </div>
+
+            <div class="footer-col">
+                <h4>Contacto</h4>
+                <p>Email: info@eduglobal.com</p>
+                <p>Tel: +52 999 123 4567</p>
+                <p>Dirección: Mérida, Yucatán, México</p>
+            </div>
+        </div>
+
+        <div class="footer-bottom">
+            <p>&copy; <?php echo date("Y"); ?> EduGlobal - Todos los derechos reservados.</p>
+        </div>
+    </footer>
+</body>
+</html>
